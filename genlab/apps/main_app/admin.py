e0877d59069775b7613d450e1c55e41a8609eaed
msheverdin648 <msheverdin648@gmail.com>
2020-10-21 22:24:29 +0300

    make db and models

--- genlab/apps/main_app/admin.py
+++ genlab/apps/main_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import (
+    ResearchMethod,
+    ResearchType,
+    Research,
+    HeaderSlid,
+    QuestionsAnswers,
+    About,
+    Partners,
+    News,
+    )
 
-# Register your models here.
+
+
+admin.site.register(ResearchMethod)
+admin.site.register(ResearchType)
+admin.site.register(Research)
+admin.site.register(HeaderSlid)
+admin.site.register(QuestionsAnswers)
+admin.site.register(About)
+admin.site.register(Partners)
+admin.site.register(News)
--- genlab/apps/main_app/models.py
+++ genlab/apps/main_app/models.py
@@ -1,44 +1,139 @@
 from django.db import models
 
-'''
-Класс Слайда
-    должно быть поле с выбором к какой странице слайд принадлежит
-    заголовок слайдера
-    картинка доктора(не обязательна)
-    текст слайдера
-    кнопка с ссылкой
-    ссылка
+
+
+class ResearchMethod(models.Model):
+
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Метод исследования'
+        verbose_name_plural = 'Методы исследования'
     
-Класс Исследование
-    берет в себя виды исследования
-
-Класс Вид исследования
-    берет в себя методы ииследования
-
-Класс Метод исследования
-    название 
-    срок
-    цена
-    ссылка на само исследование
-
-Класс с Вопрос Ответ
-    должно быть поле(галочка) подтверждающая показывать ли его на главной
+    name = models.CharField(("Название метода"), max_length=500)
+    period = models.IntegerField(("Срок исследования"))
+    price = models.DecimalField(("Цена исследования"), max_digits=12, decimal_places=2)
 
 
-Класс About 
-    поля с информацией:
-        имя поля
-        краткое описание
-        полное описание
+    def __str__(self):
+        return self.name
+    
 
-Класс partners
-    Ссылка на сайт партнера(не обязательна)
-    Логотип партнера
 
-Класс News
-    Картинка новости
-    название новости
-    текст новости
-    дата новости
-    ссылка на новость
-'''
+class ResearchType(models.Model):
+
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Вид исследования'
+        verbose_name_plural = 'Виды исследования'
+    
+    name = models.CharField(("Название вида исследования"), max_length=500)
+    methods = models.ManyToManyField(ResearchMethod, verbose_name=("Методы исследования"))
+
+    def __str__(self):
+        return self.name
+
+class Research(models.Model):
+
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Исследование'
+        verbose_name_plural = 'Исследования'
+
+    name = models.CharField(("Название исследования"), max_length=150)
+    types = models.ManyToManyField(ResearchType, verbose_name=("Входящие в него виды исследвания"))
+
+
+    def __str__(self):
+        return self.name
+
+class HeaderSlid(models.Model):
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Слайд хедера'
+        verbose_name_plural = 'Слайды хедера'
+
+
+    HOME = 'home'
+    RESEARCHES = 'researches'
+    QUESTIONS = 'questions'
+    COOPERATION = 'cooperation'
+
+    CHOICE_GROUP = {
+        (HOME, 'Главная страница'),
+        (RESEARCHES, 'Исследования'),
+        (QUESTIONS, 'Как сдать анализы'),
+        (COOPERATION, 'Сотрудничество'),
+    } 
+
+    slide_header = models.CharField(("Заголовок слайда"), max_length=500)
+    slide_text = models.TextField(("Текст слайда"))
+    page = models.CharField(("Страница на которой отображать данный слайд"), choices=CHOICE_GROUP, default=HOME,  max_length=50)
+    image = models.ImageField(("Фото отобржаемое на слайдере"), upload_to='img', null=True, blank=True)
+    button_text = models.CharField(("Текст кнопки(если есть)"), max_length=50, null=True, blank=True)
+    button_link = models.CharField(("Ссылка кнопки"), max_length=50 , null=True, blank=True)
+    link_text = models.CharField(("Текст ссылки(если есть)"), max_length=50, null=True, blank=True)
+    link_url = models.CharField(("URL ссылки"), max_length=50 , null=True, blank=True)
+
+    def __str__(self):
+        return self.slide_header
+
+
+class QuestionsAnswers(models.Model):
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Часто задаваемый вопрос/ответ'
+        verbose_name_plural = 'Часто задаваемые вопросы/ответы'
+
+    question = models.CharField(("Вопрос"), max_length=500)
+    answer = models.TextField(("Ответ"))
+    show_home = models.BooleanField(("Отображение на главной странице"))
+
+    def __str__(self):
+        return self.question
+
+class About(models.Model):
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Карточка блока подробнее о нас'
+        verbose_name_plural = 'Карточки блока подробнее о нас'
+
+    name = models.CharField(("Заголовок карточки"), max_length=150)
+    short_text = models.CharField(("Краткое описание"), max_length=500)
+    full_text = models.TextField(("Полное описание(без краткого)"))
+
+    def __str__(self):
+        return self.name
+
+class Partners(models.Model):
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Партнер'
+        verbose_name_plural = 'Партнеры'
+
+    name = models.CharField(("Название партнера"), max_length=256)
+    img = models.ImageField(("Картинка логотипа"), upload_to='img')
+    link = models.CharField(("Ссылка на сайт партнера"), max_length=256, null=True, blank=True)
+
+class News(models.Model):
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+
+    title = models.CharField(("Заголовок новости"), max_length=256)
+    text = models.TextField(("Текст новости"))
+    img = models.ImageField(("Картинка новости"), upload_to='img')
+    date = models.DateField(("Дата новости"), auto_now=False, auto_now_add=False)
+    link = models.CharField(("Ссылка на новость"), max_length=256)
+
+    def __str__(self):
+        return self.title
+
