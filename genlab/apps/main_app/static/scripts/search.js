eb389b87360e56b1b1ff748c4025cf0cd5862227
msheverdin648 <msheverdin648@gmail.com>
2020-11-17 23:45:12 +0300

    make sidebar and cards turning

--- genlab/apps/main_app/static/scripts/search.js
+++ genlab/apps/main_app/static/scripts/search.js
@@ -4,14 +4,14 @@ $(document).ready(function(){
     $('.search__list').children().remove(('li.search__list__item'))
   }
 
-  function append(response){
+  function append(response, element){
     let range = response.types.length
     if (range > 6){
       range = 6
     }
 
     for (let index = 0; index < range; index++) {
-      $('.search__list').append(
+      $(element).children('.search__list').append(
         '<li class="search__list__item"><a class="search__list__link" href="/research/'+ response.types[index].slug + '">' + response.types[index].type + '</a></li>'    
       )
     }
@@ -27,7 +27,7 @@ $(document).ready(function(){
       type: 'get',
       success: function(response){
         clean()
-        append(response)
+        append(response, '#search' )
       },
       error: function(){
         clean()
@@ -40,9 +40,37 @@ $(document).ready(function(){
   }
 
 
+  function side_send_get(){
+
+    let serializedData = $('#sidebar-search').serialize();
+
+    $.ajax({
+      url: $('#sidebar-search').data('url'),
+      data: serializedData,
+      type: 'get',
+      success: function(response){
+        clean()
+        append(response, '#sidebar-search' )
+      },
+      error: function(){
+        clean()
+        $('.sidebar__list').append(
+        '<li class="search__list__item search__list__item_not-found">Исследований не найдено</li>'    
+        )
+      }
+    })
+
+  }
+
+
   $('#search').on('input', function(){
     setTimeout(send_get, 1500)
   })
+
+  $('#sidebar-search').on('input', function(){
+    setTimeout(side_send_get, 1500)
+  })
+
 
 
 
